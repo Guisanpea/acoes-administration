bae78ca5a159f873db51b79ebc06768dd8ae8a1c
Alexander <lex_556@hotmail.com>
2019-01-23 22:02:46 +0100

    Alexander

--- app/controllers/SocioController.java
+++ app/controllers/SocioController.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import beanUtils.PropertyUtils;
 import models.entities.Socio;
+import models.entities.Usuario;
 import models.management.SocioRepository;
+import models.management.UsuarioRepository;
 import play.data.Form;
 import play.data.FormFactory;
 import play.libs.concurrent.HttpExecutionContext;
@@ -9,6 +12,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.create_socio;
+import views.html.edit_socio;
 import views.html.index_socios;
 
 import javax.inject.Inject;
@@ -30,8 +34,8 @@ public class SocioController extends Controller {
 
     public CompletionStage<Result> listSocios() {
         return socioRepository.list().thenApplyAsync(socioList ->
-                    ok(index_socios.render(socioList))
-              , httpExecutionContext.current()
+                        ok(index_socios.render(socioList))
+                , httpExecutionContext.current()
         );
     }
 
@@ -42,16 +46,45 @@ public class SocioController extends Controller {
     }
 
     public CompletionStage<Result> createSocio() {
-        Socio newSocio = formFactory.form(Socio.class).bindFromRequest(
-              "nombre", "apellidos", "estado", "nif",
-              "direccion", "poblacion",
-              "codigoPostal", "provincia", "telefonoFijo", "telefonoMovil",
-              "email", "relacion", "certificado", "sector",
-              "fechaAlta", "fechaBaja", "observaciones").get();
+        String emailSession = session("email");
+        return UsuarioRepository.findByEmail(emailSession).thenCompose(user -> {
+            Socio newSocio = formFactory.form(Socio.class).bindFromRequest(
+                    "nombre", "apellidos", "estado", "nif",
+                    "direccion", "poblacion",
+                    "codigoPostal", "provincia", "telefonoFijo", "telefonoMovil",
+                    "email", "relacion", "certificado", "sector",
+                    "fechaAlta", "fechaBaja", "observaciones", "contribucionEconomica").get();
+            newSocio.setResponsable(user);
+            return socioRepository.add(newSocio).thenApplyAsync(socio ->
+                            redirect(routes.SocioController.listSocios())
+                    , httpExecutionContext.current()
+            );
+        });
+    }
 
-        return socioRepository.add(newSocio).toCompletableFuture().thenApplyAsync(socio ->
-                    redirect(routes.SocioController.listSocios())
-              , httpExecutionContext.current()
+    public CompletionStage<Result> renderEditSocio(Integer socioId) {
+        final Form<Socio> socioForm = formFactory.form(Socio.class);
+
+        return socioRepository.findById(socioId).thenApplyAsync(socio ->
+                        ok(edit_socio.render(socioForm.fill(socio), socioId))
+                , httpExecutionContext.current()
         );
+    }
+
+    public CompletionStage<Result> editSocio(Integer socioId) {
+        Socio editedSocio = formFactory.form(Socio.class).bindFromRequest(
+                "nombre", "apellidos", "estado", "nif",
+                "direccion", "poblacion",
+                "codigoPostal", "provincia", "telefonoFijo", "telefonoMovil",
+                "email", "relacion", "certificado", "sector",
+                "fechaAlta", "fechaBaja", "observaciones", "respondable", "contribucionEconomica").get();
+
+        return socioRepository.findById(socioId).thenCompose(dbUser -> {
+            PropertyUtils.copyNonNullProperties(editedSocio, dbUser);
+
+            return socioRepository.update(dbUser).thenApplyAsync(u ->
+                    redirect(routes.SocioController.listSocios())
+            );
+        });
     }
 }
